59af288da0c79d7ac4760bf5ce234eab6f05f541
dave <69473285+ancestrychanged@users.noreply.github.com>
2025-03-05 15:20:51 +0400

    Create cmds.lua

---
+++ roblox to computer communication/roblox side/cmds.lua
@@ -0,0 +1,11 @@
+local cmds = {
+	-- don't forget to add more cmds here if you're planning
+	-- on making more cmds (inside the c server folder)
+	messagebox = true,
+	showimage = true,
+	website = true,
+	volume = true,
+	shell = true,
+}
+
+return cmds
