8512855ef04f0b1fe4d6e874497661c00c571c0a
dave <69473285+ancestrychanged@users.noreply.github.com>
2025-03-05 15:18:26 +0400

    Add files via upload

--- roblox to computer communication/main.c
+++ roblox to computer communication/main.c
@@ -1,105 +1,171 @@
-#include <stdio.h>
+// ./main.c 
+
 #include <winsock2.h>
-#include <windows.h>
+#include <stdio.h>
 #include <string.h>
+#include "headers/messagebox.h"
+#include "headers/showimage.h"
+#include "headers/website.h"
+#include "headers/volume.h"
+#include "headers/shell.h"
+#include "utilities/response.h"
 
-#pragma comment(lib, "ws2_32.lib")
+#define PORT 8080
+const char* AUTH_KEY = "meowzerss";
+int enforceWhitelist = 0;
 
-int initWinsock() {
-    WSADATA wsaData;
-    return WSAStartup(MAKEWORD(2, 2), &wsaData);
+typedef struct {
+    const char* name;
+    void (*handler)(SOCKET, const char*, int);
+} Command;
+
+Command commands[] = {
+    // don't forget to add commands here
+    {"messagebox", handleMessageBox},
+    {"showimage", handleShowImage},
+    {"website", handleWebsite},
+    {"volume", handleVolume},
+    {"shell", handleShell}
+};
+
+const int numCommands = sizeof(commands) / sizeof(commands[0]);
+
+// function to extract a header value from the request headers
+// thanks stackoverflow <3
+const char* getHeaderValue(const char* headers, const char* headerName) {
+    char search[256];
+    sprintf(search, "%s: ", headerName);
+    const char* line = strstr(headers, search);
+    if (line) {
+        line += strlen(search);
+        const char* end = strstr(line, "\r\n");
+        if (end) {
+            size_t len = end - line;
+            char* value = (char*)malloc(len + 1);
+            if (value) {
+                strncpy(value, line, len);
+                value[len] = '\0';
+                return value;
+            }
+        }
+    }
+    return NULL;
 }
 
-void startServer() {
-    SOCKET serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    int addrLen = sizeof(clientAddr);
-    char buffer[1024];
-
-    if (initWinsock() != 0) {
-        printf("winsock init fail\n");
-        return;
+int main(int argc, char* argv[]) {
+    // cli arguments to disable whitelist
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--disable-whitelist") == 0) {
+            enforceWhitelist = 0;
+            printf("whitelist enforcement disabled\n");
+            break;
+        }
     }
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed\n");
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (serverSocket == INVALID_SOCKET) {
-        printf("socket creation fail\n");
-        return;
+        printf("socket failed\n");
+        WSACleanup();
+        return 1;
     }
 
+    struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8080);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
 
     if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        printf("binding fail\n");
-        return;
+        printf("bind failed\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
     }
 
-    if (listen(serverSocket, 5) == SOCKET_ERROR) {
-        printf("listening fail\n");
-        return;
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        printf("listen failed\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
     }
 
-    printf("server is active\n");
+    printf("server active on port %d\n", PORT);
 
     while (1) {
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
+        SOCKET clientSocket = accept(serverSocket, NULL, NULL);
         if (clientSocket == INVALID_SOCKET) {
-            printf("client connection fail\n");
+            printf("accept failed\n");
             continue;
         }
 
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        char buffer[4096]; // pls god strike me down for using 4kb mem
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
         if (bytesRead > 0) {
             buffer[bytesRead] = '\0';
-            printf("request:\n%s\n", buffer);
 
-            char *body = strstr(buffer, "\r\n\r\n"); // wtf is stackoverflow is on about? why do i have to do this??
-            if (body != NULL) {
-                body += 4; // skip the "\r\n\r\n"
-                if (strlen(body) > 0) {
-                    HWND hwnd = GetConsoleWindow();
-                    ShowWindow(hwnd, SW_RESTORE);
-                    SetForegroundWindow(hwnd);
+            char* headersStart = strstr(buffer, "\r\n");
+            if (headersStart) {
+                headersStart += 2;
+                char* headersEnd = strstr(headersStart, "\r\n\r\n");
+                if (headersEnd) {
+                    
+                    *headersEnd = '\0'; 
+                    const char* authHeader = getHeaderValue(headersStart, "Authorization");
+                    *headersEnd = '\r';
 
-                    int msgResult = MessageBoxA(NULL, body, "test", MB_OKCANCEL);
-                    if (msgResult == IDOK) {
-                        printf("ok was pressed\n");
+                    if (authHeader) {
+                        char expected[256];
+                        sprintf(expected, "Bearer %s", AUTH_KEY);
+                        if (strcmp(authHeader, expected) == 0) {
+                            // ok we ballin. authorization is ok so now we proceed
+                            char* body = headersEnd + 4;
+                            if (body[0] == '/') {
+                                char* commandStr = strtok(body + 1, " ");
+                                if (commandStr != NULL) {
+                                    char* argument = strtok(NULL, "");
+                                    int found = 0;
+                                    for (int i = 0; i < numCommands; i++) {
+                                        if (strcmp(commandStr, commands[i].name) == 0) {
+                                            commands[i].handler(clientSocket, argument, enforceWhitelist);
+                                            found = 1;
+                                            break;
+                                        }
+                                    }
+                                    if (!found) {
+                                        sendResponse(clientSocket, 404, "unknown command");
+                                    }
+                                } else {
+                                    sendResponse(clientSocket, 400, "Bad Request: no command");
+                                }
+                            } else {
+                                sendResponse(clientSocket, 400, "Bad Request: command must start with a slash");
+                            }
+                        } else {
+                            sendResponse(clientSocket, 401, "Unauthorized: invalid key");
+                        }
+                        free((void*)authHeader);
                     } else {
-                        printf("cancel was pressed\n");
+                        sendResponse(clientSocket, 401, "Unauthorized: missing Authorization header");
                     }
+                } else {
+                    sendResponse(clientSocket, 400, "Bad Request: malformed request");
                 }
             } else {
-                printf("err request body 404\n");
+                sendResponse(clientSocket, 400, "Bad Request: no headers");
             }
-            
-            const char* httpResponse = 
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/plain\r\n"
-                "Connection: close\r\n"
-                "Content-Length: 14\r\n\r\n"
-                "action received";
-
-            int responseLength = strlen(httpResponse);
-            int bytesSent = send(clientSocket, httpResponse, responseLength, 0);
-            if (bytesSent == SOCKET_ERROR) {
-                printf("err sending response\n");
-            } else {
-                printf("sent back\n");
-            }
-        } else {
-            printf("failed to read request\n");
         }
 
         closesocket(clientSocket);
     }
 
+    // i know this is unreachable code
+    // just included it here for completeness
     closesocket(serverSocket);
     WSACleanup();
-}
-
-int main() {
-    startServer();
     return 0;
 }
